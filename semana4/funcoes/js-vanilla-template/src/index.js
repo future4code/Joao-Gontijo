2b9ac3cb7f325a54d03a4d41fcd9b1e99f8fff44
luzum <jpedro.gontijo@gmail.com>
2021-04-26 16:04:33 -0300

    Exercicios do dia 26

---
+++ semana4/funcoes/js-vanilla-template/src/index.js
@@ -0,0 +1,98 @@
+/*EXERCICIOS DE LEITURA
+1A) Vão ser impressos os resultados 10 e 50
+B) Se se retirasse os dois console.log, a função ainda funcionaria, porém não imprimiria nada no console.
+
+
+2A)Serão impressos os nomes "Darvas" e "Caio"
+B)Será impresso "Amanda", "Caio"
+
+
+3) A função cria um array a partir dos números pares do array anterior e eleva cada um ao quadrado. 
+Um nome para a função poderia ser arrayParAoQuadrado
+*/
+
+// EXERCICIOS ESCRITA DE CÓDIGO
+
+// 4A)
+
+// function sobreMim() {
+//     console.log('Eu sou o João, tenho 25 anos, moro em Belo Horizonte e sou desempregado.')
+// }
+// sobreMim()
+
+// B)
+
+// function sobreMimDeNovo (meuNome, minhaIdade, minhaCidade, estudoSimNao) {
+//     if(estudoSimNao){
+//         estudante ='sou estudante.'
+//     } else {
+//         estudante ='não sou estudante.'
+//     }
+//     console.log (`Eu sou ${meuNome}, tenho ${minhaIdade} anos, moro em ${minhaCidade} e ${estudoSimNao}`)
+ 
+
+// }
+
+// sobreMimDeNovo('João', 25, 'Belo Horizonte', false)
+
+
+//5A)
+
+// function somaDois(primeiroNumero,segundoNumero) {
+//     return primeiroNumero + segundoNumero 
+// }
+// console.log(somaDois(7,3))
+
+//5B)
+// function maiorOuMenor (numero1, numero2) {
+//     if (numero1 > numero2)
+//     return true
+//     else return false
+// }
+
+// console.log(maiorOuMenor(8,9))
+
+//5C)
+// function dezMensagens(mensagem) {
+//     for(let i = 0; i < 10; i++){
+//         console.log(mensagem)
+//     }
+// }
+
+// dezMensagens('Alou')
+
+//6A)
+
+// const array = [10, 23, 45, 78, 90, 52, 35, 67, 84, 22]
+// let i = 0;
+
+// function tamanhoDoArray(listaNumeros) {
+//     while (i< listaNumeros.length) {
+//         i++
+//     }
+// console.log('Tanto de números do array', i)
+    
+// }
+// tamanhoDoArray(array)
+    
+
+//B)
+// const array = [10, 23, 45, 78, 90, 52, 35, 67, 84, 22]
+
+// function parOuNao(numero){
+//     if (numero%2 === 0)
+//     return true
+//     else return false
+
+// }
+
+// console.log(parOuNao(2))
+
+
+//C)
+
+
+
+
+
+
