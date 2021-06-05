050c2a88d38028c183736ed2910dad8f6dffa915
luzum <jpedro.gontijo@gmail.com>
2021-06-05 11:35:47 -0300

    astromatch com quase nada de estilizaçao

---
+++ semana9/astromatch/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Main from './Components/Main/Main';
+import ResetButton from './Components/ResetButton/ResetButton';
+
+function App() {
+  return (
+    <div >
+      <Main/>
+      <ResetButton />
+    </div>
+  );
+}
+
+export default App;
