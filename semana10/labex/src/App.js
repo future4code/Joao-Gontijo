018b11dc0848540a27ea1e26e854fca1a138203a
luzum <jpedro.gontijo@gmail.com>
2021-06-07 19:12:42 -0300

    Primeiro PR do projeto

---
+++ semana10/labex/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header>oi</header>
+      <footer>Teste</footer>
+    </div>
+  );
+}
+
+export default App;
